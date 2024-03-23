117fec391634faa2c24dc2ca088036171e808457
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2024-03-22 20:25:52 -0400

    Added linkedin data

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,13 @@
-const API =
+const YOUTUBE_API =
   "https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9";
 
-const content = null || document.getElementById("content");
+const LINKEDIN_API =
+  "https://linkedin-api8.p.rapidapi.com/?username=miguel-aldo-balderrama-vaca-b69a04122";
 
-const options = {
+const content = null || document.getElementById("content");
+const titleH1 = null || document.querySelector("div[id='profileTitle']");
+
+const youtubeOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "dae2529088mshf13a4d119490960p164159jsnfbcbb21728c5",
@@ -11,7 +15,15 @@ const options = {
   },
 };
 
-async function fetchData(urlApi) {
+const linkedinOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "dae2529088mshf13a4d119490960p164159jsnfbcbb21728c5",
+    "X-RapidAPI-Host": "linkedin-api8.p.rapidapi.com",
+  },
+};
+
+async function fetchData(urlApi, options) {
   const response = await fetch(urlApi, options);
   const data = await response.json();
   return data;
@@ -19,7 +31,42 @@ async function fetchData(urlApi) {
 
 (async () => {
   try {
-    const videos = await fetchData(API);
+    const linkedinData = await fetchData(LINKEDIN_API, linkedinOptions);
+    console.info(linkedinData);
+    let view = `
+      <h1
+          class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span id="firstName" class="block xl:inline">${
+            linkedinData.firstName
+          } ${linkedinData.lastName}</span>
+          <span id="lastName" class="block text-indigo-600 xl:inline">@mabalderramav</span>
+      </h1>
+      <p
+          class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          ${linkedinData.summary}
+      </p>
+      <p
+          class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          <strong> Open to work: ${linkedinData.isOpenToWork
+            .toString()
+            .toUpperCase()} </strong>
+      </p>
+      <p
+          class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          <strong> Is hiring? ${linkedinData.isHiring
+            .toString()
+            .toUpperCase()} </strong>
+      </p>
+    `;
+    titleH1.innerHTML = view;
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+(async () => {
+  try {
+    const videos = await fetchData(YOUTUBE_API, youtubeOptions);
     let view = `
 	${videos.items
     .map(
